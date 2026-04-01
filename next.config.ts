dd6a50eca1841ae1be53efcf034068440ddad461
Afsana Fazil <afsanafazil03@gmail.com>
2026-04-02 03:12:13 +0530

    updated next.config file

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,11 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://udemy-backend-j7bd.onrender.com/api';
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${apiUrl}/:path*`,
       },
     ];
   },
